e373e8aa9906b16ba6cb21115324c1288c5e2c2f
Quinn Avila <qavila@drw.com>
2016-01-09 10:45:47 -0600

    added index.html and js structure

---
+++ js/controller.js
@@ -0,0 +1,8 @@
+angular.module('myNoteControllers', [])
+  .controller('myNoteController', function($scope) {
+    $scope.message = "";
+    $scope.left = function() {
+      return 100-$scope.message.length;};
+    $scope.clear = function() {$scope.message = "";};
+    $scope.save = function() {alert("Note Saved");};
+  });
